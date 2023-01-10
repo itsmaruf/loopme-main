0eeed97eedcaf75cf627899bce86177d3ab69a1a
itsmaruf <maruf@corexlab.dev>
2023-01-10 12:12:35 +0600

    app router added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,22 @@ export const routes = createBrowserRouter(
         {
             path: "/",
             element: <h2 className="text-5xl">Home</h2>
+        },
+        {
+            path: "/user",
+            element: <h2 className="text-5xl">User</h2>
+        },
+        {
+            path: "/grower",
+            element: <h2 className="text-5xl">Grower</h2>
+        },
+        {
+            path: "/admin",
+            element: <h2 className="text-5xl">Admin</h2>
+        },
+        {
+            path: "/*",
+            element: <h2 className="text-5xl">Not Found</h2>
         }
     ]
 )
